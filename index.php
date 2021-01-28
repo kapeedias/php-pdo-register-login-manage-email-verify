759e49bc88e3fbd749038477e57243bce1a1e1d3
Sai Deepak <78069444+kapeedias@users.noreply.github.com>
2021-01-27 23:19:40 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Sai Deepak and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.79.0">
     <title>Login</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
   <body class="text-center">
-    <main class="form-signin">
+    <div class="row">
+    <div class="col-md-4">
       <form method="POST" enctype="multipart/form-data" action="index.php">
         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
         <label for="inputEmail" class="visually-hidden">Email address</label>
@@ -25,7 +26,9 @@
         <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
         <p class="mt-5 mb-3 text-muted">App by <a href="https://github.com/kapeedias/" target="_blank">kapeedias</a></p>
       </form>
-    </main>
+      </div>
+    </div>
   </body>
 </html>
+
 
