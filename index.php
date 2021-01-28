5515618de153b5dbfece65770df4010806990fdc
Sai Deepak <78069444+kapeedias@users.noreply.github.com>
2021-01-27 23:20:46 -0800

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="description" content="">
     <meta name="author" content="Sai Deepak and Bootstrap contributors">
     <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" crossorigin="anonymous">
+<script src="assets/js/bootstrap.min.js" crossorigin="anonymous"></script>
   </head>
   <body class="text-center">
     <div class="row">
